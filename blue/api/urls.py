2228cddef47069c61891f30cadc76b81fd2f0456
Emanuele Scarabattoli <em.scarabattoli@gmail.com>
2019-10-06 22:54:35 +0200

    Fixes in api urls

--- blue/api/urls.py
+++ blue/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import GraphQLView
+from graphene_django.views import GraphQLView
 
 urlpatterns = [path("", csrf_exempt(GraphQLView.as_view(graphiql=True)))]
